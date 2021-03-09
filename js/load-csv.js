3c0ba5147573abfc07e843d96b14a0ec66b6c112
Santiago Acevedo <sacevedor@unbosque.edu.co>
2021-03-09 18:29:23 -0500

    supporting csv loading

--- js/load-csv.js
+++ js/load-csv.js
@@ -1,9 +1,13 @@
-
+var allData;
 // Loading a CSV using D3
 d3.dsv( ";", "../data/pets-citizens.csv" )
   .then( function( data ) {
-    console.log( data.length );    
+    allData = data;
+    printData();   
   } )
   .catch( function( error ) {
     // handle error   
   } );
+function printData(){
+	console.log(allData);
+}
