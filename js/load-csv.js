47eda103e563939678b6b7667ea894a98183f775
Camilo Gomez <fcgomez@unbosque.edu.co>
2021-03-09 20:17:22 -0500

    Creating table in page1

--- js/load-csv.js
+++ js/load-csv.js
@@ -9,5 +9,14 @@ d3.dsv( ";", "../data/pets-citizens.csv" )
     // handle error   
   } );
 function printData(){
-	console.log(allData);
+	console.log("Hola");
+	table = document.getElementById("table");
+for(var i = 1; i < table.rows.length; i++)
+  {
+    for(var j = 0; j < table.rows[i].cells.length; j++)
+    { 
+      table.rows[i].cells[0].innerHTML = allData[i-1]["Microchip"];
+    }
+
+}
 }
