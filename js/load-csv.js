c77e1b8e5907a0b2c89aa7261ff0038f040c27d1
Santiago Acevedo <sacevedor@unbosque.edu.co>
2021-03-09 22:06:20 -0500

    Loading the table

--- js/load-csv.js
+++ js/load-csv.js
@@ -9,14 +9,6 @@ d3.dsv( ";", "../data/pets-citizens.csv" )
     // handle error   
   } );
 function printData(){
-	console.log("Hola");
-	table = document.getElementById("table");
-for(var i = 1; i < table.rows.length; i++)
-  {
-    for(var j = 0; j < table.rows[i].cells.length; j++)
-    { 
-      table.rows[i].cells[0].innerHTML = allData[i-1]["Microchip"];
-    }
-
+	console.log(allData);
 }
 }
