21f393e218c9eb9a0e8e64b619f5274549c8e992
Camilo Gomez <fcgomez@unbosque.edu.co>
2021-03-14 18:28:09 -0500

    Pagination

--- js/load-csv.js
+++ js/load-csv.js
@@ -3,12 +3,49 @@ var allData;
 d3.dsv( ";", "../data/pets-citizens.csv" )
   .then( function( data ) {
     allData = data;
-    printData();   
+    printTable(); 
+    printData();  
   } )
   .catch( function( error ) {
     // handle error   
   } );
-function printData(){
-	console.log(allData);
-}
-}
+function printTable(){
+
+   let tablaProducto = document.getElementById('table');
+        let cuerpoTabla = document.createElement('tbody');
+
+        allData.forEach(p => {
+     
+            let fila = document.createElement('tr');
+            
+            let td = document.createElement('td');
+            td.innerText = p.microchip;
+            fila.appendChild(td);
+
+            td = document.createElement('td');
+            td.innerText = p.species;
+            fila.appendChild(td);
+
+            td = document.createElement('td');
+            td.innerText = p.sex;
+            fila.appendChild(td);
+
+             td = document.createElement('td');
+            td.innerText = p.size;
+            fila.appendChild(td);
+
+ td = document.createElement('td');
+            td.innerText = p.potentDangerous;
+            fila.appendChild(td);
+             td = document.createElement('td');
+            td.innerText = p.neighborhood;
+            fila.appendChild(td);
+
+
+            cuerpoTabla.appendChild(fila);
+        });
+
+        tablaProducto.appendChild(cuerpoTabla);
+  }
+
+
