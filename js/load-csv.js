bfd095d0b46652834945d114b29cde451d536392
Santiago Acevedo <sacevedor@unbosque.edu.co>
2021-03-16 20:24:21 -0500

    finishing the creating pet form

--- js/load-csv.js
+++ js/load-csv.js
@@ -1,68 +1,75 @@
 var allData;
 // Loading a CSV using D3
 d3.dsv( ";", "../data/pets-citizens.csv" )
-  .then( function( data ) {
-    allData = data;
-    printTable(); 
-    printData();  
-  } )
-  .catch( function( error ) {
+.then( function( data ) {
+  allData = data;
+  printTable(); 
+  printData();  
+} )
+.catch( function( error ) {
     // handle error   
   } );
 function printTable(){
 
-   let tablaProducto = document.getElementById('table');
-        let cuerpoTabla = document.createElement('tbody');
+ let tablaProducto = document.getElementById('table');
+ let cuerpoTabla = document.createElement('tbody');
 
-        allData.forEach(p => {
-     
-            let fila = document.createElement('tr');
-            
-            let td = document.createElement('td');
-            td.innerText = p.microchip;
-            fila.appendChild(td);
+ allData.forEach(p => {
 
-            td = document.createElement('td');
-            td.innerText = p.species;
-            fila.appendChild(td);
+  let fila = document.createElement('tr');
 
-            td = document.createElement('td');
-            td.innerText = p.sex;
-            fila.appendChild(td);
+  let td = document.createElement('td');
+  td.innerText = p.microchip;
+  fila.appendChild(td);
 
-             td = document.createElement('td');
-            td.innerText = p.size;
-            fila.appendChild(td);
+  td = document.createElement('td');
+  td.innerText = p.species;
+  fila.appendChild(td);
 
- td = document.createElement('td');
-            td.innerText = p.potentDangerous;
-            fila.appendChild(td);
-             td = document.createElement('td');
-            td.innerText = p.neighborhood;
-            fila.appendChild(td);
+  td = document.createElement('td');
+  td.innerText = p.sex;
+  fila.appendChild(td);
+
+  td = document.createElement('td');
+  td.innerText = p.size;
+  fila.appendChild(td);
+
+  td = document.createElement('td');
+  td.innerText = p.potentDangerous;
+  fila.appendChild(td);
+  td = document.createElement('td');
+  td.innerText = p.neighborhood;
+  fila.appendChild(td);
 
 
-            cuerpoTabla.appendChild(fila);
-        });
+  cuerpoTabla.appendChild(fila);
+});
 
-        tablaProducto.appendChild(cuerpoTabla);
-  }
+ tablaProducto.appendChild(cuerpoTabla);
+}
 
-  function addPet(){
-    
+function addPet(){
+
   console.log("Hola");
-  var microchip = document.getElementById("micro").value;
-  var species = document.getElementById("Species").value;
-  var sex = document.getElementById("sex").value;
-  var size = document.getElementById("size").value;
-  var potentDangerous = document.getElementById("danger").value;
-  var neighborhood = document.getElementById("localidad").value;
+ var imagen = document.getElementById("file").files[0];
+    var reader = new FileReader();
+    if (file) {
+    reader.readAsDataURL(imagen); 
+  }else{
+    }
+      var microchip = document.getElementById("micro").value;
+      var species = document.getElementById("Species").value;
+      var sex = document.getElementById("sex").value;
+      var size = document.getElementById("size").value;
+      var potentDangerous = document.getElementById("danger").value;
+      var neighborhood = document.getElementById("localidad").value;
+      var race = document.getElementById("raza").value;
+      var owner = document.getElementById("own").value;
+      var address = document.getElementById("direccion").value;
   // alert(microchip + " " + species + " " + sex + " " + size + " " + potentDangerous + " " + neighborhood )
   console.log(allData[allData.length - 1]);
-    allData.push({"microchip" : microchip, "species" : species, "sex" : sex, "size" : size, "potentDangerous" : potentDangerous, "neighborhood" : neighborhood});
+  allData.push({"microchip" : microchip, "species" : species, "sex" : sex, "size" : size, "potentDangerous" : potentDangerous, "neighborhood" : neighborhood, "Race" : race, "Owner" : owner, "Address" : address, "Photo" : imagen});
   console.log(allData[allData.length - 1]);
 
 }
-  
-
 
